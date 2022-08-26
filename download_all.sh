6d8f90376c2e1eaf948f0e8466495053f2574d42
Halyjo <35629262+Halyjo@users.noreply.github.com>
2022-08-26 12:59:27 +0200

    Changed download to not use cmip6 data.

--- download_all.sh
+++ download_all.sh
@@ -8,31 +8,33 @@ echo icenet/gen_masks.py finished >> log.txt
 echo ----------------- >> log.txt
 
 echo ----------------- >> log.txt
-python icenet/download_sic_data.py >> log.txt
+python icenet/download_sic_data.py
 echo icenet/download_sic_data.py finished >> log.txt
 echo ----------------- >> log.txt
 
 echo ----------------- >> log.txt
-./download_era5_data_in_parallel.sh >> log.txt
+./download_era5_data_in_parallel.sh
 echo download_era5_data_in_parallel.sh finished >> log.txt
 echo ----------------- >> log.txt
 
-echo ----------------- >> log.txt
-./download_cmip6_data_in_parallel.sh >> log.txt
-echo download_cmip6_data_in_parallel.sh finished >> log.txt
-echo ----------------- >> log.txt
+######### Probably not needed (did not contribute with a lot in performance and is a lot of data.) #############
+# echo ----------------- >> log.txt
+# ./download_cmip6_data_in_parallel.sh
+# echo download_cmip6_data_in_parallel.sh finished >> log.txt
+# echo ----------------- >> log.txt
 
+################################################################################################################
 echo ----------------- >> log.txt
-./rotate_wind_data_in_parallel.sh >> log.txt
+./rotate_wind_data_in_parallel.sh
 echo rotate_wind_data_in_parallel.sh finished >> log.txt
 echo ----------------- >> log.txt
 
 echo ----------------- >> log.txt
-./download_seas5_forecasts_in_parallel.sh >> log.txt
+./download_seas5_forecasts_in_parallel.sh
 echo download_seas5_forecasts_in_parallel.sh finished >> log.txt
 echo ----------------- >> log.txt
 
 echo ----------------- >> log.txt
-python icenet/biascorrect_seas5_forcasts.py >> log.txt
-echo icenet/biascorrect_seas5_forcasts.py finished >> log.txt
+python icenet/biascorrect_seas5_forecasts.py >> log.txt
+echo icenet/biascorrect_seas5_forecasts.py finished >> log.txt
 echo ----------------- >> log.txt
