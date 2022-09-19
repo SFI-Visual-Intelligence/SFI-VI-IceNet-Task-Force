2880cb2fbb62ce22858c28d7771027caa4134913
Halyjo <35629262+Halyjo@users.noreply.github.com>
2022-09-19 18:35:09 +0200

    add cmip6 to preprocess again.

--- preprocess_data.sh
+++ preprocess_data.sh
@@ -4,8 +4,8 @@ echo "icenet/gen_data_loader_config.py done" >> log.txt && \
 python3 icenet/preproc_icenet_data.py && \
 echo "icenet/preproc_icenet_data.py done" >> log.txt && \
 ## Making the program faster
-python3 icenet/gen_numpy_obs_train_val_datasets.py && \
-echo "icenet/gen_numpy_obs_train_val_datasets.py done" >> log.txt && \
-python3 icenet/gen_tfrecords_obs_train_val_datasets.py && \
-echo "icenet/gen_tfrecords_obs_train_val_datasets.py done" >> log.txt && \
+# python3 icenet/gen_numpy_obs_train_val_datasets.py && \
+# echo "icenet/gen_numpy_obs_train_val_datasets.py done" >> log.txt && \
+# python3 icenet/gen_tfrecords_obs_train_val_datasets.py && \
+# echo "icenet/gen_tfrecords_obs_train_val_datasets.py done" >> log.txt && \
 echo "Preprocessing done!" >> log.txt
