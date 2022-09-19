fc8c7aa652e65c6ef41186d70be5e682c1bb91d8
Halyjo <35629262+Halyjo@users.noreply.github.com>
2022-09-19 18:57:45 +0200

    update history-files, logs, input figures, small details in dockerfile and download_cmip....sh

--- download_cmip6_data_in_parallel.sh
+++ download_cmip6_data_in_parallel.sh
@@ -1,17 +1,50 @@
 #!/bin/bash
 
-mkdir -p logs/cmip6_download_logs/
+## Not sure if this is needed here to
+python copy_cdsapirc_file_to_container_home.py
+
+mkdir -p logs/cmip6_download_logs/ &&
+
+# # MRI-ESM2.0
+# ############ THIS HOLTS FOR SOME REASON ##############
+# python icenet/download_cmip6_data.py  --source_id MRI-ESM2-0 --member_id r2i1p1f1 2>&1 | tee -a logs/cmip6_download_logs/MRI_r2i1p1f1.txt 
+######################################################
+
+#######################################################################################################################################
+######################################################### New try #####################################################################
+
+# # python icenet/download_cmip6_data.py  --source_id MRI-ESM2-0 --member_id r1i1p1f1 2>&1 | tee -a logs/cmip6_download_logs/MRI_r1i1p1f1.txt
+# # python icenet/download_cmip6_data.py  --source_id MRI-ESM2-0 --member_id r2i1p1f1 2>&1 | tee -a logs/cmip6_download_logs/MRI_r2i1p1f1.txt
+# python icenet/download_cmip6_data.py  --source_id MRI-ESM2-0 --member_id r3i1p1f1 2>&1 | tee -a logs/cmip6_download_logs/MRI_r3i1p1f1.txt
+# # python icenet/download_cmip6_data.py  --source_id MRI-ESM2-0 --member_id r4i1p1f1 2>&1 | tee -a logs/cmip6_download_logs/MRI_r4i1p1f1.txt
+# # python icenet/download_cmip6_data.py  --source_id MRI-ESM2-0 --member_id r5i1p1f1 2>&1 | tee -a logs/cmip6_download_logs/MRI_r5i1p1f1.txt
+
+# # # EC-Earth3
+# # python icenet/download_cmip6_data.py  --source_id EC-Earth3 --member_id r2i1p1f1 2>&1 | tee -a logs/cmip6_download_logs/EC_r2i1p1f1.txt
+# # python icenet/download_cmip6_data.py  --source_id EC-Earth3 --member_id r7i1p1f1 2>&1 | tee -a logs/cmip6_download_logs/EC_r7i1p1f1.txt
+# # python icenet/download_cmip6_data.py  --source_id EC-Earth3 --member_id r10i1p1f1 2>&1 | tee -a logs/cmip6_download_logs/EC_r10i1p1f1.txt
+# # python icenet/download_cmip6_data.py  --source_id EC-Earth3 --member_id r12i1p1f1 2>&1 | tee -a logs/cmip6_download_logs/EC_r12i1p1f1.txt
+# # python icenet/download_cmip6_data.py  --source_id EC-Earth3 --member_id r14i1p1f1 2>&1 | tee -a logs/cmip6_download_logs/EC_r14i1p1f1.txt 
+
+#######################################################################################################################################
+#######################################################################################################################################
+
+# #######################################################################################################################################
+# ######################################################### Old #########################################################################
 
 # MRI-ESM2.0
-python3 icenet/download_cmip6_data.py  --source_id MRI-ESM2-0 --member_id r1i1p1f1 > logs/cmip6_download_logs/MRI_r1i1p1f1.txt 2>&1 &
-python3 icenet/download_cmip6_data.py  --source_id MRI-ESM2-0 --member_id r2i1p1f1 > logs/cmip6_download_logs/MRI_r2i1p1f1.txt 2>&1 &
-python3 icenet/download_cmip6_data.py  --source_id MRI-ESM2-0 --member_id r3i1p1f1 > logs/cmip6_download_logs/MRI_r3i1p1f1.txt 2>&1 &
-python3 icenet/download_cmip6_data.py  --source_id MRI-ESM2-0 --member_id r4i1p1f1 > logs/cmip6_download_logs/MRI_r4i1p1f1.txt 2>&1 &
-python3 icenet/download_cmip6_data.py  --source_id MRI-ESM2-0 --member_id r5i1p1f1 > logs/cmip6_download_logs/MRI_r5i1p1f1.txt 2>&1 &
+python icenet/download_cmip6_data.py  --source_id MRI-ESM2-0 --member_id r1i1p1f1 > logs/cmip6_download_logs/MRI_r1i1p1f1.txt 2>&1 &
+python icenet/download_cmip6_data.py  --source_id MRI-ESM2-0 --member_id r2i1p1f1 > logs/cmip6_download_logs/MRI_r2i1p1f1.txt 2>&1 &
+python icenet/download_cmip6_data.py  --source_id MRI-ESM2-0 --member_id r3i1p1f1 > logs/cmip6_download_logs/MRI_r3i1p1f1.txt 2>&1 &
+python icenet/download_cmip6_data.py  --source_id MRI-ESM2-0 --member_id r4i1p1f1 > logs/cmip6_download_logs/MRI_r4i1p1f1.txt 2>&1 &
+python icenet/download_cmip6_data.py  --source_id MRI-ESM2-0 --member_id r5i1p1f1 > logs/cmip6_download_logs/MRI_r5i1p1f1.txt 2>&1 &
 
 # EC-Earth3
-python3 icenet/download_cmip6_data.py  --source_id EC-Earth3 --member_id r2i1p1f1 > logs/cmip6_download_logs/EC_r2i1p1f1.txt 2>&1 &
-python3 icenet/download_cmip6_data.py  --source_id EC-Earth3 --member_id r7i1p1f1 > logs/cmip6_download_logs/EC_r7i1p1f1.txt 2>&1 &
-python3 icenet/download_cmip6_data.py  --source_id EC-Earth3 --member_id r10i1p1f1 > logs/cmip6_download_logs/EC_r10i1p1f1.txt 2>&1 &
-python3 icenet/download_cmip6_data.py  --source_id EC-Earth3 --member_id r12i1p1f1 > logs/cmip6_download_logs/EC_r12i1p1f1.txt 2>&1 &
-python3 icenet/download_cmip6_data.py  --source_id EC-Earth3 --member_id r14i1p1f1 > logs/cmip6_download_logs/EC_r14i1p1f1.txt 2>&1 &
+python icenet/download_cmip6_data.py  --source_id EC-Earth3 --member_id r2i1p1f1 > logs/cmip6_download_logs/EC_r2i1p1f1.txt 2>&1 &
+python icenet/download_cmip6_data.py  --source_id EC-Earth3 --member_id r7i1p1f1 > logs/cmip6_download_logs/EC_r7i1p1f1.txt 2>&1 &
+python icenet/download_cmip6_data.py  --source_id EC-Earth3 --member_id r10i1p1f1 > logs/cmip6_download_logs/EC_r10i1p1f1.txt 2>&1 &
+python icenet/download_cmip6_data.py  --source_id EC-Earth3 --member_id r12i1p1f1 > logs/cmip6_download_logs/EC_r12i1p1f1.txt 2>&1 &
+python icenet/download_cmip6_data.py  --source_id EC-Earth3 --member_id r14i1p1f1 > logs/cmip6_download_logs/EC_r14i1p1f1.txt 2>&1 &
+
+# #######################################################################################################################################
+# #######################################################################################################################################
---
+++ train_models.sh
@@ -0,0 +1,5 @@
+for i in {42..84}
+do
+    python icenet/train_icenet.py --seed=$i
+done
+
