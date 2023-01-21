30f8711813cf077eac6bdca534c66a6e83218d36
Iver Martinsen <iver.martinsen@outlook.com>
2023-01-21 18:28:47 +0100

    Created scripts for reproducing plot 7 in supplementary materials

---
+++ icenet/experimental/create_dummy_dataframe.py
@@ -0,0 +1,60 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.join(os.getcwd(), "icenet"))
+
+import pandas as pd
+import numpy as np
+import utils
+import config
+from tqdm import tqdm
+
+destionation_folder = "icenet/experimental/results"
+
+
+def create_dummy_dataframe():
+    """
+    Create a dummy dataframe with the same structure as the permute-and-predict results dataframe.
+    """
+    all_ordered_variable_names = dataloader.determine_variable_names()
+
+    n_forecast_months = 6
+    heldout_start = "2012-01-01"
+    heldout_end = "2012-12-01"
+
+    leadtimes = np.arange(1, n_forecast_months + 1)
+    model_numbers = range(1)
+    target_dates = pd.date_range(start=heldout_start, end=heldout_end, freq="MS")
+
+    multi_index = pd.MultiIndex.from_product(
+        [all_ordered_variable_names, model_numbers, leadtimes, target_dates],
+        names=["Model number", "Leadtime", "Forecast date", "Variable"],
+    )
+
+    results_df = pd.DataFrame(
+        index=multi_index, columns=["Accuracy drop (%)"], dtype=np.float32
+    )
+
+    for model_number in tqdm(model_numbers):
+        for _, varname in enumerate(tqdm(all_ordered_variable_names, leave=False)):
+            for target_date in target_dates:
+                for leadtime in leadtimes:
+                    results_df.loc[
+                        model_number, leadtime, target_date, varname
+                    ] = np.random.uniform(0, 100)
+
+    return results_df
+
+
+if __name__ == "__main__":
+
+    dataloader_ID = "2021_06_15_1854_icenet_nature_communications"
+    dataloader_config_fpath = os.path.join(
+        config.dataloader_config_folder, dataloader_ID + ".json"
+    )
+    dataloader = utils.IceNetDataLoader(dataloader_config_fpath)
+
+    os.makedirs(destionation_folder, exist_ok=True)
+
+    data_frame = create_dummy_dataframe()
+    data_frame.to_csv(os.path.join(destionation_folder, "dummy_dataframe.csv"))
---
+++ icenet/experimental/produce_figure_7.py
@@ -0,0 +1,94 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.join(os.getcwd(), "icenet"))
+import config
+import utils
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+
+### User input
+####################################################################
+
+dataloader_ID = "2021_06_15_1854_icenet_nature_communications"
+figure_destionation_folder = "icenet/experimental/figures"
+results_folder = "icenet/experimental/results"
+filename = "dummy_dataframe.csv"
+
+### Load results dataframe
+####################################################################
+
+dataloader_config_fpath = os.path.join(
+    config.dataloader_config_folder, dataloader_ID + ".json"
+)
+dataloader = utils.IceNetDataLoader(dataloader_config_fpath)
+results_df = pd.read_csv(os.path.join(results_folder, filename))
+
+### Produce Figure 7
+####################################################################
+
+# List variable names as they appear in figure 7, supplementary material
+all_ordered_variable_names = dataloader.determine_variable_names()
+
+# Compute mean over all models and forecast dates
+mean_results_df = results_df.groupby(["Leadtime", "Variable"]).mean()
+
+# Reorder rows and columns
+mean_results_heatmap = (
+    mean_results_df.reset_index()
+    .pivot(index="Variable", columns="Leadtime")
+    .reindex(all_ordered_variable_names)["Accuracy drop (%)"]
+)
+
+# Reset index to make variable names appear in the heatmap
+mean_results_df = mean_results_df.reset_index()
+
+cbar_kws = {}
+cbar_kws["label"] = "Accuracy change (%)"
+
+verbose_varnames = []
+short_varnames = mean_results_heatmap.index.values.astype("str")
+for varname in short_varnames:
+    verbose_varname = utils.make_varname_verbose_any_leadtime(varname)
+    verbose_varnames.append(verbose_varname)
+mean_results_heatmap.index = verbose_varnames
+
+with plt.rc_context(
+    {
+        "font.size": 9,
+        "axes.labelsize": 9,
+        "ytick.labelsize": 9,
+        "xtick.labelsize": 9,
+    }
+):
+    fig, ax = plt.subplots(figsize=(6, 9))
+
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="10%", pad=0.1)
+
+    sns.heatmap(
+        data=mean_results_heatmap,
+        annot=True,
+        annot_kws=dict(fontsize=7),
+        fmt=".2f",
+        ax=ax,
+        cmap="RdBu_r",
+        center=0.0,
+        cbar_kws=cbar_kws,
+        vmin=-0.5,
+        cbar_ax=cax,
+    )
+    ax.set_xlabel("Lead time (months)")
+    ax.set_ylabel("Input variable name")
+    cax.set_frame_on(True)
+    ax.set_frame_on(True)
+    plt.tight_layout()
+
+os.makedirs(figure_destionation_folder, exist_ok=True)
+
+plt.savefig(os.path.join(figure_destionation_folder, "supp_fig7.png"))
+plt.savefig(os.path.join(figure_destionation_folder, "supp_fig7.pdf"))
+plt.close()
